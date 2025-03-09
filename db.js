599397275524bb8d1cfbe08558b20af1ffb702ef
DaniyarS1703 <daniyars535@gmail.com>
2025-03-09 08:56:40 +0500

    Add files via upload

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Pool } = require('pg');
-const dotenv = require('dotenv');
-
-dotenv.config();
+require('dotenv').config();
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
+    connectionString: process.env.DATABASE_URL + "?application_name=myapp&client_encoding=UTF8",
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
